8cac2b958f59c1565ec5d162ecd9ea9b602790c7
sharpxiajun <sharpxiajun2008@163.com>
2020-11-20 17:55:36 +0800

    提交Layout

--- src/regsiter-components.js
+++ src/regsiter-components.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import {
-  ConfigProvider
+  ConfigProvider,
+  Layout,
+  Menu,
+  Breadcrumb,
+  Icon
 } from 'ant-design-vue'
 
 Vue.use(ConfigProvider)
+Vue.use(Layout)
+Vue.use(Menu)
+Vue.use(Breadcrumb)
+Vue.use(Icon)
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,18 +1,116 @@
 <template>
-  <div>
-    <span style="font-size: 60px;color:green;">排版布局</span>
-    <transition name="fadeinLeft" mode="out-in">
-      <router-view></router-view>
-    </transition>
-  </div>
+  <a-layout class="layout">
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :default-selected-keys="['2']"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          nav 1
+        </a-menu-item>
+        <a-menu-item key="2">
+          nav 2
+        </a-menu-item>
+        <a-menu-item key="3">
+          nav 3
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu
+          mode="inline"
+          :default-selected-keys="['1']"
+          :default-open-keys="['sub1']"
+          :style="{ height: '100%', borderRight: 0 }"
+        >
+          <a-sub-menu key="sub1">
+            <span slot="title"><a-icon type="user" />subnav 1</span>
+            <a-menu-item key="1">
+              option1
+            </a-menu-item>
+            <a-menu-item key="2">
+              option2
+            </a-menu-item>
+            <a-menu-item key="3">
+              option3
+            </a-menu-item>
+            <a-menu-item key="4">
+              option4
+            </a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <span slot="title"><a-icon type="laptop" />subnav 2</span>
+            <a-menu-item key="5">
+              option5
+            </a-menu-item>
+            <a-menu-item key="6">
+              option6
+            </a-menu-item>
+            <a-menu-item key="7">
+              option7
+            </a-menu-item>
+            <a-menu-item key="8">
+              option8
+            </a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <span slot="title"><a-icon type="notification" />subnav 3</span>
+            <a-menu-item key="9">
+              option9
+            </a-menu-item>
+            <a-menu-item key="10">
+              option10
+            </a-menu-item>
+            <a-menu-item key="11">
+              option11
+            </a-menu-item>
+            <a-menu-item key="12">
+              option12
+            </a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+          <transition name="fadeinLeft" mode="out-in">
+            <router-view></router-view>
+          </transition>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
   export default {
-    name: 'layout'
+    name: 'layout',
+    data() {
+      return {
+        collapsed: false
+      }
+    }
   }
 </script>
 
-<style lang="less" scoped>
-
+<style lang="less">
+  .layout {
+    .logo {
+      width: 120px;
+      height: 31px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 16px 28px 16px 0;
+      float: left;
+    }
+  }
 </style>
